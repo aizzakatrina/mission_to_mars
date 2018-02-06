5849ca52ed374ee596a074a73ded8ff59d4a534d
aizzakatrina <aizzakatrina@live.com>
2018-02-05 22:32:42 -0800

    updated

---
+++ scrape_mars.py
@@ -0,0 +1,12 @@
+
+
+# Converting your Jupyter notebook into a Python script with a function called scrape.
+
+
+
+# Create a route called /scrape that will import your scrape_mars.py script and call your scrape function.
+# Store the return value in Mongo as a Python dictionary.
+# Create a root route / that will query your Mongo database and pass and display the mars data into an HTML template.
+
+# Use Pymongo for CRUD applications for your database. 
+# You can simply overwrite the existing document each time the /scrape url is visited and new data is obtained.
